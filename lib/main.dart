52dfeb663a34ca614aa2be8b58f2222e262238c6
hsotnas76 <hsotnas76@gmail.com>
2022-12-07 04:21:38 +0545

    provide two data

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,27 @@ void main() {
 
 final currentDate = Provider<DateTime>(((ref) => DateTime.now()));
 
+class Counter extends StateNotifier<int> {
+  Counter(super.state);
+  void increment() => state++;
+}
+
+class NewCounter extends StateNotifier<int> {
+  NewCounter(super.state);
+  void increment() {
+    debugPrint('date time increment');
+    state++;
+  }
+}
+
+final dateProvider = StateNotifierProvider<NewCounter, int>(((ref) {
+  return NewCounter(1);
+}));
+
+final counterProvider = StateNotifierProvider<Counter, int>(((ref) {
+  return Counter(0);
+}));
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -27,23 +48,32 @@ class MyHomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final date = ref.watch(currentDate);
+    debugPrint('build');
     return Scaffold(
-      appBar: AppBar(
-        title: Text(date.toIso8601String()),
-      ),
+      appBar: AppBar(title: Consumer(builder: (context, ref, child) {
+        final date = ref.watch(dateProvider);
+        return Text(date.toString());
+      })),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
+          children: <Widget>[
+            Consumer(
+              builder: (context, ref, child) {
+                final count = ref.watch(counterProvider);
+                return Text(
+                  'You have pushed the button this many times: $count',
+                );
+              },
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: (() {
+          ref.read(counterProvider.notifier).increment();
+          ref.read(dateProvider.notifier).increment();
+        }),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
