701cf37a574eff0ccd1fa991607544623f3d8b36
hsotnas76 <hsotnas76@gmail.com>
2022-12-15 20:04:27 +0545

    setup and initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:equatable/equatable.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_demo/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   EquatableConfig.stringify = true;
   runApp(const ProviderScope(child: MyApp()));
 }
